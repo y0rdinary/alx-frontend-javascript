24f5c6324f52158fc8113ca6b6aaaf456eb3a3fc
y0rdinary <84769643+y0rdinary@users.noreply.github.com>
2022-04-10 13:12:09 +0300

    frontend javascript

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,11 +1,11 @@
-export default function createReportObject(employeesList){
-    return {
-        allEmployees: {
-            ...employeesList,
-        },
-        getNumberOfDepartments(empList){
-            return Object.keys(empList).length;
-        },
-    };
-    
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments(empList){
+      return Object.keys(empList).length;
+    },
+  };
+
 }
