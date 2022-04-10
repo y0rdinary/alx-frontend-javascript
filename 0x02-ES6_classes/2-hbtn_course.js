8a97ae1e0adb041afadc20f3d4ac93f2413373c2
y0rdinary <84769643+y0rdinary@users.noreply.github.com>
2022-04-10 18:24:10 +0300

    es6 classes

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,30 +1,30 @@
 export default class HolbertonCourse {
-    constructor(name, length, students) {
-      this.name = name;
-      this.length = length;
-      this.students = students;
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  // name
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
     }
-  
-    // name
-    get name() {
-      return this._name;
-    }
-  
-    set name(value) {
-      if (typeof value !== 'string') {
-        throw new TypeError('Name must be a string');
-      }
-      this._name = value;
-    }
-  
-    // length
-    get length() {
-      return this._length;
-    }
-  
-    set length(value) {
-      if (typeof value !== 'number') {
-        throw new TypeError('Length must be a number');
+    this._name = value;
+  }
+
+  // length
+  get length() {
+    return this._length;
+  }
+
+  set length(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Length must be a number');
       }
       this._length = value;
     }
