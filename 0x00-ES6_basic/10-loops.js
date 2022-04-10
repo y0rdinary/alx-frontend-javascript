a0829f0918a8bf0d37fd1f98d50678f0c5ded398
y0rdinary <84769643+y0rdinary@users.noreply.github.com>
2022-04-10 13:10:13 +0300

    frontend javascript

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
-    for (const idx in array) {
-      const value = array[idx];
-      array[idx] = appendString + value;
-    }
-  
-    return array;
+  // eslint-disable-next-line
+	for (const value of array) {
+    // eslint-disable-next-line
+		array[array.indexOf(value)] = appendString + value;
   }
+
+  return array;
+}
