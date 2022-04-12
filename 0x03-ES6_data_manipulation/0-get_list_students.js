8d8ade5bd2e97595d8109dba218f1dbf094cd734
y0rdinary <84769643+y0rdinary@users.noreply.github.com>
2022-04-12 17:08:48 +0300

    data maniplation

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,6 +1,6 @@
 const studList = ['id: 1, fisrtName: Guillaume, location: San Francisco', 'id: 2, firstName: James, location: Columbia', 'id: 5, firstName:Serena, location: San Francisco'] ;
-createListOfObjects(studList);
+getListStudents(studList);
 
-export default function createListOfObjects(studList) {
+export default function getListStudents(studList) {
   console.log(studList);
 }
