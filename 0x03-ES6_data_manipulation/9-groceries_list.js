16ae6be88b943886116ca51e2cef439cd164b087
y0rdinary <84769643+y0rdinary@users.noreply.github.com>
2022-04-14 13:22:16 +0300

    data manipulation

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,9 +1,9 @@
 export default function groceriesList() {
-  return new Map ([
+  return new Map([
     ['Apples', 10],
     ['Tomatoes', 10],
     ['Pasta', 1],
     ['Rice', 1],
-    ['Banana', 5]
+    ['Banana', 5],
   ]);
 }
