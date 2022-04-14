e0f3b01a260516c7d83a494ba7260a537b042549
y0rdinary <84769643+y0rdinary@users.noreply.github.com>
2022-04-14 12:56:20 +0300

    data manipulation

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -6,8 +6,8 @@ export default function cleanSet(set, startString) {
     for (const element of set) {
       if (element && element.startsWith(startString)) {
         array.push(element.slice(startString.length));
+      }
     }
-  }
   text = array.join('-');
   }
 return text;
