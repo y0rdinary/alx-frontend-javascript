8e54ba29278f0edcd7da795bef769b01a86ff85d
y0rdinary <84769643+y0rdinary@users.noreply.github.com>
2022-04-14 09:41:12 +0300

    data manipulation

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,4 +1,6 @@
-import getListStudents from "./0-get_list_students";
 export default function getListStudentIds(getListStudents) {
-    return getListStudents.map(getListStudentIds(id, index, getListStudents), )
+  if(Array.isArray(list)) {
+  return list.map((obj) => obj,id);
+}
+  return [];
 }
