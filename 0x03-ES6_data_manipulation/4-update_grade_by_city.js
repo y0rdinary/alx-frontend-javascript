36e92739f2a5b79b189bf2206fa08f562cb21b8b
y0rdinary <84769643+y0rdinary@users.noreply.github.com>
2022-04-14 11:36:49 +0300

    data manipulation

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,3 +1,3 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  return students.filter((stud) => stud.location === city).map((stud) => ({ ...stud, grade: newGrades.filter((grades) => grades.studentId === stud.id).map((grades) => grades.grade) [0] || 'N/A' }));
+  return students.filter((stud) => stud.location === city).map((stud) => ({ ...stud, grade: newGrades.filter((grades) => grades.studentId === stud.id).map((grades) => grades.grade)[0] || 'N/A' }));
 }
