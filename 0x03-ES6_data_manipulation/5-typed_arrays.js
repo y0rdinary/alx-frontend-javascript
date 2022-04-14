e36b13872959695c81166aa45fc0a6bb1ef81056
y0rdinary <84769643+y0rdinary@users.noreply.github.com>
2022-04-14 12:13:33 +0300

    data manipulation

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,9 @@
-export default function createInt8TypedArray() {
+export default function createInt8TypedArray(length, positon, value) {
   const buffer = new ArrayBuffer(length);
   const dataview = new DataView(length);
 
   if(positon >= length) throw new Error('Position is outside range');
-  dataview.setInt8(positon, value);
+    dataview.setInt8(positon, value);
 
   return dataview;
 }
